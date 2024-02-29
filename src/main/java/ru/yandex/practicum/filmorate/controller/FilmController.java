9624d27dcb3290fe4c08b41a4fe22065a95de21f
anbananova <jiveumba99@yandex.ru>
2024-02-29 14:07:35 +0300

    feat: User, Film, controllers

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,76 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.FilmAlreadyExistException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RestController
 public class FilmController {
+    private final List<Film> films = new ArrayList<>();
+
+    @GetMapping("/films")
+    public List<Film> getFilms() {
+        log.debug("Текущее количество фильмов: {}", films.size());
+        return films;
+    }
+
+    @PostMapping(value = "/films")
+    public Film create(@Valid @RequestBody Film film) {
+        if (film.getDescription().length() > 200) {
+            throw new ValidationException("Описание фильмов превышает 200 символов. Длина: "
+                    + film.getDescription().length());
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.parse("1895-12-28"))) {
+            throw new ValidationException("Дата релиза не может быть в раньше 28 декабря 1895 года: "
+                    + film.getReleaseDate());
+        }
+        if (film.getDuration() < 0) {
+            throw new ValidationException("Продолжительность фильма должна быть положительной: "
+                    + film.getDuration());
+        }
+
+        film.setId(films.size() + 1);
+
+        if (films.contains(film)) {
+            throw new FilmAlreadyExistException("Фильм уже был добавлен: " + film);
+        }
+
+        log.debug("Текущий фильм: {}", film);
+        films.add(film);
+        return film;
+    }
+
+    @PutMapping(value = "/films")
+    public Film update(@Valid @RequestBody Film film) {
+        if (film.getDescription().length() > 200) {
+            throw new ValidationException("Описание фильмов превышает 200 символов. Длина: "
+                    + film.getDescription().length());
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.parse("1895-12-28"))) {
+            throw new ValidationException("Дата релиза не может быть в раньше 28 декабря 1895 года: "
+                    + film.getReleaseDate());
+        }
+        if (film.getDuration() < 0) {
+            throw new ValidationException("Продолжительность фильма должна быть положительной: "
+                    + film.getDuration());
+        }
+
+        if (films.contains(film)) {
+            films.remove(film);
+        } else {
+            throw new ValidationException("Такого фильма не существует: " + film);
+        }
+
+        log.debug("Текущий фильм: {}", film);
+        films.add(film);
+        return film;
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,69 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.UserAlreadyExistException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.util.*;
+
+@Slf4j
+@RestController
+public class UserController {
+    private final List<User> users = new ArrayList<>();
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        log.debug("Текущее количество пользователей: {}", users.size());
+        return users;
+    }
+
+    @PostMapping(value = "/users")
+    public User create(@Valid @RequestBody User user) {
+        if (user.getLogin().contains(" ")) {
+            throw new ValidationException("У пользователя пробелы в логине: " + user.getLogin());
+        }
+        if (user.getName() == null || user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем: " + user.getBirthday());
+        }
+
+        user.setId(users.size() + 1);
+
+        if (users.contains(user)) {
+            throw new UserAlreadyExistException("Пользователь уже был добавлен: " + user);
+        }
+
+        log.debug("Текущий пользователь: {}", user);
+        users.add(user);
+        return user;
+    }
+
+    @PutMapping(value = "/users")
+    public User update(@Valid @RequestBody User user) {
+        if (user.getLogin().contains(" ")) {
+            throw new ValidationException("У пользователя пробелы в логине: " + user.getLogin());
+        }
+        if (user.getName() == null || user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("Дата рождения не может быть в будущем: " + user.getBirthday());
+        }
+
+        if (users.contains(user)) {
+            users.remove(user);
+        } else {
+            throw new ValidationException("Такого пользователя не существует: " + user);
+        }
+
+        log.debug("Текущий пользователь: {}", user);
+        users.add(user);
+        return user;
+    }
+}
